1a645218e88333cab623b83398de7428c63d22f6
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-27 23:44:27 +0400

    Добавьте файлы проекта.

---
+++ FirstDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string st = "var_1__Int";
+            char c = firstDigit(st);
+            Console.WriteLine(c);
+        }
+
+        static char firstDigit(string inputString)
+        {
+            List<char> list = new List<char>();
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] >= (char)48 && inputString[i] <= (char)57)
+                {
+                    list.Add(inputString[i]);
+                }
+            }
+            return list[0];
+        }
+    }
+}
